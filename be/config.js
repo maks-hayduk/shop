19414e1b84e0f9cf5c785bf30ec8ef4fa15af7e8
Maks Hayduk <maksym.h@nerdysoft.com>
2019-11-24 17:36:42 +0200

    Established connection with local PostgreSQL, added bable, nodemon, jwt

--- be/config.js
+++ be/config.js
@@ -1,9 +1,12 @@
 import dotenv from 'dotenv';
 
-const { DB_PASSWORD } = dotenv.config().parsed;
+const { DB_PASSWORD, DB_USERNAME, DB_NAME, SECRET_KEY } = dotenv.config().parsed;
 
 const config = {
-  dbpassword: DB_PASSWORD
+  dbpassword: DB_PASSWORD,
+  dbusername: DB_USERNAME,
+  dbname: DB_NAME,
+  secretKey: SECRET_KEY
 }
 
 export default Object.freeze(config);
---
+++ be/middleware.js
@@ -0,0 +1,33 @@
+import jwt from 'jsonwebtoken';
+
+import config from './config';
+
+const checkToken = (req, res, next) => {
+  const token = req.headers['x-access-token'] || req.headers['authorization'];
+  if (token.startsWith('Bearer ')) {
+    token = token.slice(7, token.length);
+  }
+
+  if (token) {
+    jwt.verify(token, config.secretKey, (err, decoded) => {
+      if (err) {
+        return res.json({
+          success: false,
+          message: 'Token is not valid'
+        });
+      } else {
+        req.decoded = decoded;
+        next();
+      }
+    });
+  } else {
+    return res.json({
+      success: false,
+      message: 'Auth token is not supplied'
+    });
+  }
+};
+
+export default {
+  checkToken
+}
